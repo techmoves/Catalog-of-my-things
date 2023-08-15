cda201c5cd9d8d684868345f6ab7bfcd67cee743
AleWaweru <ngashalex8@gmail.com>
2023-08-15 12:25:16 +0300

    Add can-achieve and move-to-achieve methods

--- item.rb
+++ item.rb
@@ -1,30 +1,43 @@
 class Item
-    attr_reader :title, :published_date
-    attr_accessor :archived
-    def initialize(title, published_date)
-      @title = title
-      @published_date = published_date
-      @archived = false
-      @genres = []
-      @sources = []
-      @labels = []
-    end
-    def add_genre(genre)
-      @genres << genre
-    end
-    def add_source(source)
-      @sources << source
-    end
-    def add_label(label)
-      @labels << label
-    end
-    def add_author(author)
-      @author << author
-    end
-    def can_be_archived?
-    end
+  attr_reader :title, :published_date
+  attr_accessor :archived
 
-    def move_to_archive
-      end
+  def initialize(title, published_date)
+    @title = title
+    @published_date = published_date
+    @archived = false
+    @genres = []
+    @sources = []
+    @labels = []
+  end
+
+  def add_genre(genre)
+    @genres << genre
+  end
+
+  def add_source(source)
+    @sources << source
+  end
+
+  def add_label(label)
+    @labels << label
+  end
+
+  def add_author(author)
+    @author << author
+  end
+
+  def can_be_archived?
+    years_since_publication = Time.now.year - @published_date.year
+    years_since_publication >= 10
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts 'Item has been moved to the archive.'
+    else
+      puts 'Item cannot be archived yet.'
     end
   end
+end
